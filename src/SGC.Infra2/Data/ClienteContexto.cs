21210f65eccfdb827e691ff0b5d04e63a4d40a84
Marcelo França <marcelo.jlf23@outlook.com>
2020-01-08 22:49:41 -0300

    Cria classe de cliente de contexto e adiciona migrations

---
+++ src/SGC.Infra2/Data/ClienteContexto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGC.AplicatinoCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.Infra2.Data
+{
+    public class ClienteContexto : DbContext
+    {
+        public ClienteContexto(DbContextOptions<ClienteContexto> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Cliente");
+        }
+    }
+}
--- src/SGC.UI.Web/Startup.cs
+++ src/SGC.UI.Web/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using SGC.UI.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SGC.Infra2.Data;
 
 namespace SGC.UI.Web
 {
@@ -37,8 +38,12 @@ namespace SGC.UI.Web
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddDbContext<ClienteContexto>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
